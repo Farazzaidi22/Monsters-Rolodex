c1e01cb285bf7ac9aecdbad34842b51c5cff1256
Farazzaidi22 <farazkhalid05@yahoo.com>
2021-12-08 04:24:10 +0500

    implemented states and JSX code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react/cjs/react.production.min';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+
+  constructor(){
+    super()
+
+    this.state = {
+      monsters: [
+        {
+          name: 'Mephisto',
+          id: 'asc1'
+        },
+        {
+          name: 'Dracula',
+          id: 'asc2'
+        },
+        {
+          name: 'Zombie',
+          id: 'asc3'
+        },
+      ]
+    } 
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.monsters.map( monster => (
+          
+          <h1 key={monster.id}> {monster.name} </h1>
+        
+          )
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
