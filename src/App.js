49b769e6ed9a17ac472016d6c7b633c3c39b45ac
Farazzaidi22 <farazkhalid05@yahoo.com>
2021-12-08 16:49:28 +0500

    added bg styles

--- src/App.js
+++ src/App.js
@@ -38,6 +38,8 @@ class App extends Component{
 
     return (
       <div className="App">
+
+        <h1> Monster Rolodex </h1>
         
         <SearchBox 
             placeholder='search for monsters'
